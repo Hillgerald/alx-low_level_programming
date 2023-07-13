9fbbc9afea6b6813e79722b8a926cc9fd2614df8
Hillary Mayunga <mayungahillary@gmail.com>
2023-07-13 13:35:18 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that displays text in a line
+ * using puts
+ * Return:0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
