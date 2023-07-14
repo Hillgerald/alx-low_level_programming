d1515d5cf9460a699ed0ef0111c3ab23f449be3c
Hillary Mayunga <mayungahillary@gmail.com>
2023-07-14 11:50:48 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - a program to print numbers to base 16
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char e;
+
+	int b;
+
+	e = 'a';
+	b = '0';
+	while
+		(b < 10) {
+			putchar(b + '0');
+			b++;
+		}
+	while
+		(e <= 'f') {
+			putchar(e);
+			e++;
+		}
+	putchar('\n');
+	return (0);
+}
+
+
