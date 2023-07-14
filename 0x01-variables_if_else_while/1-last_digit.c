6ee5e8bb36565441dd46ae64e329654b610aadb4
Hillary Mayunga <mayungahillary@gmail.com>
2023-07-14 07:15:26 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,7 +12,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	m = n % 10;
+	o = n % 10;
 	if (o > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, o);
 	if (o == 0)
