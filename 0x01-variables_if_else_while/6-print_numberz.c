8986769f24a8c746c829cdf0e87f7521f9285fb3
Hillary Mayunga <mayungahillary@gmail.com>
2023-07-14 08:36:57 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - a program that prints base 10 digits
+ * Return: 0 success
+ */
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while
+		(c < 10) {
+			putchar(c + '0');
+			c++;
+		}
+	putchar(''\n');
+	return (0);
+}
