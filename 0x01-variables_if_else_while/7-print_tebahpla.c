bc8295dfffcad662655033884591644efa894a7d
Hillary Mayunga <mayungahillary@gmail.com>
2023-07-14 11:38:01 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - a program that prints alphabets in reverse
+ * Return: 0 is a success
+ */
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n);
+	return (0);
+}
