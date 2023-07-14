90505b015b2f784e2b81d8a87168ad0fb7213043
Hillary Mayunga <mayungahillary@gmail.com>
2023-07-14 08:30:09 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - a program to print all single numbers of base 10
+ * Return: 0 is a success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
