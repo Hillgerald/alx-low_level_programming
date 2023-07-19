6d13a1df1e22c6ac2a3a419e9c443c3141a2dd00
Hillary Mayunga <mayungahillary@gmail.com>
2023-07-19 16:07:54 -0700

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,11 @@
 #include "main.h"
 /**
  * add - add two values
+ * @n1: fisrt num
+ * @n2: second num
  * Return: Integer value
  */
 int add(int n1, int n2)
 {
-	return (n1 +n2);
+	return (n1 + n2);
 }
