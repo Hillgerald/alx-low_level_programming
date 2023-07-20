2ba99aab9308c41e2a6d7b4dd1972e8d875d6ce7
Hillary Mayunga <mayungahillary@gmail.com>
2023-07-20 15:27:49 -0700

    debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - a program to print a positive or negative number
+ * 0: number to be tested
+ * Return: 0 (Success)
+ */
+void positive_or_negative(int i)
+{
+        if (i < 0)
+	{
+		printf("%d is %s\n", i, "negative");
+	}
+	else if (i > 0)
+	{
+		printf("%d is %s\n", i, "positive");
+	}
+	else
+	{
+		printf("%d is %s\n", i, "zero");
+	}
+	return;
+}
