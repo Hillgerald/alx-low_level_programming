c220fe367de8e4f4da34d060e0d814db01a3a440
Hillary Mayunga <mayungahillary@gmail.com>
2023-08-02 14:50:48 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,7 +9,7 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return(actual_prime(n, n - 1));
+	return (actual_prime(n, n - 1));
 }
 
 /**
@@ -29,5 +29,5 @@ int actual_prime(int n, int i)
 		return (1);
 	if (n % i == 0 && i > 0)
 		return (0);
-	return (actual_prime(n, i -1));
+	return (actual_prime(n, i - 1));
 }
