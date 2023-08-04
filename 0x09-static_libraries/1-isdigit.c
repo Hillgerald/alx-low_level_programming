8620e8d46d564adbea9fa3ee025747f518583794
Hillary Mayunga <mayungahillary@gmail.com>
2023-08-04 16:23:04 -0700

    libmy.a

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,5 +1,4 @@
 #include "main.h"
-                    	
 /**
  * _isdigit - checks for a digit
  * @c: int to be checked
@@ -7,5 +6,5 @@
  */
 int _isdigit(int c)
 {
-        	return (c >= '0' && c <= '9');
+	return (c >= '0' && c <= '9');
 }
--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,6 +1,5 @@
 #include "main.h"
-
-/**
+ /**
  * char *_strcpy - program copies the string pointed to by src
  * @dest: copy to
  * @src: copy from
@@ -8,17 +7,17 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-        	int a = 0;
-        	int b = 0;
+	int a = 0;
+	int b = 0;
 
-        	while (*(src + a) != '\0')
-        	{
-                    	a++;
-        	}
-        	for ( ; b < a ; b++)
-        	{
-                    	dest[b] = src[b];
-        	}
-        	dest[a] = '\0';
-        	return (dest);
+	while (*(src + a) != '\0')
+	{
+		a++;
+	}
+	for ( ; b < a ; b++)
+	{
+		dest[b] = src[b];
+	}
+	dest[a] = '\0';
+	return (dest);
 }
